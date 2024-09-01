2e3d2f5702a9136b18d4f80e669d4a1bc434ff71
Alexander <sadorsadoff@gmail.com>
2024-09-01 18:51:29 +0200

    task e13

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            require.resolve('react-refresh/babel'), 
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ReactRefreshWebpackPlugin(), 
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+};
